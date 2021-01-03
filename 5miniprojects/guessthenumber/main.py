98f4f23d84459c891debd98669a736ede2012689
Mark Crowther <mark.crowther@bjss.com>
2021-01-03 15:47:09 +0000

    Added folder and draft main.py file

---
+++ 5miniprojects/guessthenumber/main.py
@@ -0,0 +1,12 @@
+# Guess the Number
+
+import turtle
+
+# Set up the window in which everything will be displayed
+wn = turtle.Screen()
+wn.bgcolor("Black")
+wn.title("~ Guess the Number ~")
+
+# Code goes here
+
+wn.mainloop()
